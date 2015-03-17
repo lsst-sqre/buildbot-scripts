58932e37dae170a8af172a38e87a66f1692c7454
Joshua Hoblitt <jhoblitt@cpan.org>
2015-03-17 19:00:04 -0400

    (DM-2367) convert most settings.cfg.sh vars to be override-able in the env

--- settings.cfg.sh
+++ settings.cfg.sh
@@ -2,7 +2,7 @@
 
 # general
 LSSTSW=${LSSTSW:-$HOME}
-BUILD_DIR=${BUILD_DIR:-${HOME}/build}
+BUILD_DIR=${BUILD_DIR:-"${HOME}/build"}
 
 BUILDBOT_SUCCESS=0
 BUILDBOT_FAILURE=1
@@ -10,15 +10,15 @@ BUILDBOT_WARNING=2
 
 # lsstswBuild.sh
 # options passed to ./create_xlinkdocs.sh as cli options
-DOC_PUSH_USER=${DOC_PUSH_USER:-buildbot}
-DOC_PUSH_HOST=${DOC_PUSH_HOST:-lsst-dev.ncsa.illinois.edu}
-DOC_PUSH_PATH=${DOC_PUSH_PATH:-/lsst/home/buildbot/public_html/doxygen}
+DOC_PUSH_USER=${DOC_PUSH_USER:-"buildbot"}
+DOC_PUSH_HOST=${DOC_PUSH_HOST:-"lsst-dev.ncsa.illinois.edu"}
+DOC_PUSH_PATH=${DOC_PUSH_PATH:-"/lsst/home/buildbot/public_html/doxygen"}
 
 # create_xlinkdocs.sh
-DOC_REPO_URL="https://github.com/lsst/lsstDoxygen.git"
-DOC_REPO_NAME="lsstDoxygen"
-DOC_REPO_DIR=${BUILD_DIR}/${DOC_REPO_NAME}
+DOC_REPO_URL=${DOC_REPO_URL:-"https://github.com/lsst/lsstDoxygen.git"}
+DOC_REPO_NAME=${DOC_REPO_NAME:-"lsstDoxygen"}
+DOC_REPO_DIR=${DOC_REPO_DIR:-"${BUILD_DIR}/${DOC_REPO_NAME}"}
 
 # runManifestDemo.sh
-DEMO_ROOT="https://dev.lsstcorp.org/cgit/contrib/demos/lsst_dm_stack_demo.git/snapshot"
-DEMO_TGZ="lsst_dm_stack_demo-master.tar.gz"
+DEMO_ROOT=${DEMO_ROOT:-"https://dev.lsstcorp.org/cgit/contrib/demos/lsst_dm_stack_demo.git/snapshot"}
+DEMO_TGZ=${DEMO_TGZ:-"lsst_dm_stack_demo-master.tar.gz"}
